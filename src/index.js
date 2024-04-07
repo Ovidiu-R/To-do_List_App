e70e0c580594bb7769b85137b139f6b5f0a9fd04
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-04-07 21:50:52 +0100

    Create working module with grid

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
 
+document.addEventListener('DOMContentLoaded', () => {
+
+const modal = document.querySelector('dialog'); 
+const button = document.getElementById('all');
+button.addEventListener('click', () => {
+    modal.showModal();
+    modal.classList.add('active');
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modal.classList.remove('active');
+        closeModal();
+        
+    }
+});
+
+
+});
