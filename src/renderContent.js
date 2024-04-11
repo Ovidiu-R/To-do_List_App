92c32dacd72cc29039bf811044ca933f85a7547f
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-04-11 21:03:26 +0100

    Write rudimentary task renderer

--- src/renderContent.js
+++ src/renderContent.js
@@ -1,6 +1,7 @@
 const content = document.getElementById('content');
 
 export const renderContent = () => {
+    content.textContent = '';
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         const value = localStorage.getItem(key);
@@ -12,17 +13,20 @@ export const renderContent = () => {
 }
 const createElements = (key, task) => {
     const container = document.createElement('div');
-    container.setAttribute('class', '.task');
+    container.setAttribute('class', 'task');
     container.setAttribute('id', key);
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
     const title = document.createElement('p');
-    title.setAttribute('value', 'HAHAHAHAHA')
+    title.textContent = task.title;
     const details = document.createElement('button');
+    details.textContent = 'Details';
     const date = document.createElement('p');
+    date.textContent = task.date;
     const edit = document.createElement('button');
+    edit.textContent = 'edit';
     const erase = document.createElement('button');
+    erase.textContent = 'delete';
     container.append(checkbox, title, details, date, edit, erase);
-    content.textContent = '';
     content.append(container);
 }
